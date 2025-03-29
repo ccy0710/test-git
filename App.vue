63a48b5841b1165a3f6773813d5b02ad18407536
ccy-102521 <1787110056@qq.com>
2025-03-29 11:00:25 +0800

    update feature

--- App.vue
+++ App.vue
@@ -4,6 +4,7 @@
 			console.log('App Launch')
 			console.log('add dev')
 			console.log('add feature-build')
+			console.log('update feature')
 		},
 		onShow: function() {
 			console.log('App Show')
