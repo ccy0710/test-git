3ff31cbf0a58407c0ec8cae4111d01dec36fd9e2
ccy-102521 <1787110056@qq.com>
2025-03-29 11:06:25 +0800

    s

--- App.vue
+++ App.vue
@@ -4,6 +4,7 @@
 			console.log('App Launch')
 			console.log('add dev')
 			console.log('add feature-build')
+			console.log('feature update')
 		},
 		onShow: function() {
 			console.log('App Show')
